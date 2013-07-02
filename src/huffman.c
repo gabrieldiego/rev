f7c67b83a610879efe4cfda3597522859024223c
Gabriel Diego Teixeira <gabrieldiegoteixeira@gmail.com>
2013-07-02 20:15:34 -0300

    Ajuste do símbolo na lista movido para uma função separada

--- src/huffman.c
+++ src/huffman.c
@@ -1,5 +1,42 @@
 #include "huffman.h"
 
+void adjust_symbol_in_list(huffman_tree_t *ht, huffman_list_t *current) {
+  /* Avançar na lista caso o símbolo ocorrer mais vezes que o próximo */
+  while(current->bigger != NULL) {
+
+    /* Testa se simbolo adiante na lista ocorre menas vezes */
+    if(current->bigger->leaf->occurrence < current->leaf->occurrence) {
+      huffman_list_t *bigger_ptr;
+      huffman_list_t *smaller_ptr;
+
+      /* Usa estas variáveis para simplificar o código */
+      bigger_ptr  = current->bigger;
+      smaller_ptr = current->smaller;
+
+      /* Ajusta o ponteiro do nodo após o maior (se existir) */
+      if(bigger_ptr->bigger != NULL)
+        bigger_ptr->bigger->smaller = current;
+      else
+        ht->biggest = current;
+
+      /* Ajusta os ponteiros do nodo atual */
+      current->bigger = bigger_ptr->bigger;
+      current->smaller = bigger_ptr;
+
+      /* Ajusta os ponteiros do nodo maior */
+      bigger_ptr->bigger = current;
+      bigger_ptr->smaller= smaller_ptr;
+
+      /* Ajusta o ponteiro do nodo menor (se existir) */
+      if(smaller_ptr != NULL)
+        smaller_ptr->bigger = bigger_ptr;
+      else
+        ht->smallest = bigger_ptr;
+    } else
+      break;
+  }
+}
+
 int create_huffman_list(huffman_tree_t *ht, uint8_t *input, size_t len) {
   size_t i;
   huffman_leaf_t *leaf_list;
@@ -38,43 +75,7 @@ int create_huffman_list(huffman_tree_t *ht, uint8_t *input, size_t len) {
 
   for(i=0;i<len;i++) {
     ht->list[input[i]].leaf->occurrence++;
-
-    /* Avançar na lista caso o símbolo ocorrer mais vezes que o próximo */
-    while(ht->list[input[i]].bigger != NULL) {
-
-      if(ht->list[input[i]].bigger->leaf->occurrence < 
-              ht->list[input[i]].leaf->occurrence) {
-        huffman_list_t *bigger_ptr;
-        huffman_list_t *smaller_ptr;
-        huffman_list_t *current_ptr;
-
-        /* Usa estas variáveis para simplificar o código */
-        current_ptr = ht->list+input[i];
-        bigger_ptr  = current_ptr->bigger;
-        smaller_ptr = current_ptr->smaller;
-
-        /* Ajusta o ponteiro do nodo após o maior (se existir) */
-        if(bigger_ptr->bigger != NULL)
-          bigger_ptr->bigger->smaller = current_ptr;
-        else
-          ht->biggest = current_ptr;
-
-        /* Ajusta os ponteiros do nodo atual */
-        current_ptr->bigger = bigger_ptr->bigger;
-        current_ptr->smaller = bigger_ptr;
-
-        /* Ajusta os ponteiros do nodo maior */
-        bigger_ptr->bigger = current_ptr;
-        bigger_ptr->smaller= smaller_ptr;
-
-        /* Ajusta o ponteiro do nodo menor (se existir) */
-        if(smaller_ptr != NULL)
-          smaller_ptr->bigger = bigger_ptr;
-        else
-          ht->smallest = bigger_ptr;
-      } else
-        break;
-    }
+    adjust_symbol_in_list(ht,ht->list+input[i]);
   }
 
   return;
